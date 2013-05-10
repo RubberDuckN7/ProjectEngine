751196109fc50e9d64a01fae42b63989ba95035d
LF <private>
2013-05-10 13:25:31 +0200

    Testing

--- Game Engine/Game Engine/Game Engine/Game1.cs
+++ Game Engine/Game Engine/Game Engine/Game1.cs
@@ -78,7 +78,9 @@ namespace Game_Engine
                 this.Exit();
 
             // TODO: Add your update logic here
-
+            int x = 2;
+            int y = (x == 2) ? x : -1;
+            
             base.Update(gameTime);
         }
 
